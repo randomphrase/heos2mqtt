5c7d9e096768025c0fd7d3eb39c40a93738d2e54
Alastair Rankine <alastair@girby.net>
2025-12-26 14:33:09 -0500

    add cdash support

---
+++ CTestConfig.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+set(CTEST_PROJECT_NAME "heos2mqtt")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=heos2mqtt")
+set(CTEST_DROP_SITE_CDASH TRUE)
