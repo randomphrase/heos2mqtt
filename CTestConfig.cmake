c418d4c6be4a89df7d36e10f08c85024d70273f0
Alastair Rankine <alastair@girby.net>
2025-12-29 10:21:29 -0500

    exclude third-party code from coverage analysis

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,8 @@
-# SPDX-License-Identifier: MIT
-
 set(CTEST_PROJECT_NAME "heos2mqtt")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+set(CTEST_SUBMIT_URL "https://my.cdash.org/submit.php?project=heos2mqtt")
 
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=heos2mqtt")
-set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE
+  ".*/external/.*"
+  ".*/vcpkg_installed/.*"
+)
