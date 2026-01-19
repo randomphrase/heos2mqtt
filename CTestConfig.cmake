d2ab6890513eaa4e47b36b2ced781d5c72c66533
Alastair Rankine <alastair@girby.net>
2026-01-19 15:42:33 -0500

    Attempt to get coverage working

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,8 +1,3 @@
 set(CTEST_PROJECT_NAME "heos2mqtt")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 set(CTEST_SUBMIT_URL "https://my.cdash.org/submit.php?project=heos2mqtt")
-
-set(CTEST_CUSTOM_COVERAGE_EXCLUDE
-  ".*/external/.*"
-  ".*/vcpkg_installed/.*"
-)
